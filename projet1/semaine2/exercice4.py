16f9a32901c25abd01563b48ecbb53aacfcea51a
matiskudra <matis.kudra3@gmail.com>
2026-02-05 13:20:32 -0500

    update 13:20

--- projet1/semaine2/exercice4.py
+++ projet1/semaine2/exercice4.py
@@ -1,3 +1,9 @@
+import random
+
 nombre_mini = int(input("insérer un nombre plus grand que 0"))
 nombre_maxi = int(input("insérer un nombre plus grand que le précédent"))
-nombre_random = 
+nombre_random = random.randint(nombre_mini, nombre_maxi)
+print(f"Nombre le plus bas : {nombre_mini}")
+print(f"Nombre le plus haut : {nombre_maxi}")
+print()
+print(f"Nombre aléatoire généré : {nombre_random}")
