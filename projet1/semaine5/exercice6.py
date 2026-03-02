ddd3088f23f78c9ecbf05bd86008dc5b480b5116
matiskudra <matis.kudra3@gmail.com>
2026-03-02 14:27:45 -0500

    fini

--- projet1/semaine5/exercice6.py
+++ projet1/semaine5/exercice6.py
@@ -1,3 +1,12 @@
 def pyramide(hauteur):
+    nb_etoiles = 1
+    nb_espaces = hauteur - 1
     for i in range(hauteur):
-
+        for n in range(nb_espaces):
+            print(" ", end='')
+        for n in range(nb_etoiles):
+            print("*", end='')
+        nb_etoiles += 2
+        nb_espaces -= 1
+        print()
+pyramide(5)
