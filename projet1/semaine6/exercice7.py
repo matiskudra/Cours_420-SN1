9c2657d93b4537cee9457c70792499b7e6a646c7
matiskudra <matis.kudra3@gmail.com>
2026-04-16 12:48:14 -0400

    ex8

--- projet1/semaine6/exercice7.py
+++ projet1/semaine6/exercice7.py
@@ -1,1 +1,27 @@
 temperatures = [23.5, 25.4, 27.6, 21.9, 26.7, 24.6]
+
+compteur = 0
+for t in temperatures:
+    compteur += 1
+
+somme = 0
+for s in temperatures:
+    somme += s
+
+moyenne = somme / compteur
+
+min_temp = temperatures[0]
+for t in temperatures:
+    if t < min_temp:
+        min_temp = t
+max_temp = temperatures[-1]
+for t in temperatures:
+    if t > max_temp:
+        max_temp = t
+
+print(f"Températures : {temperatures}")
+print(f"nb. mesures : {compteur}")
+print(f"somme : {round(somme, 1)}")
+print(f"moyenne : {round(moyenne, 2)}")
+print(f"min : {min_temp}")
+print(f"max : {max_temp}")
